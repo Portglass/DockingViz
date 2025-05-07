ebf0105b5036b19fd3b97ebfe4786b1cd390c0ba
Martin-rev <corp.revillon@hubebi.com>
2025-05-07 18:49:15 +0200

    test

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import path
 import sys
+import os
 
 #Global Variable
 dir = path.Path(__file__).absolute()
 sys.path.append(dir.parent)
 
 print(dir.parent)
+print(os.listdir())
 
 st.session_state.RESULT_FILE = dir.parent+"\\Result_docking\\"
 st.session_state.DOCKING_FILE = dir.parent+"\\Fichier docking\\"
