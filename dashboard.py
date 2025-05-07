c55094720f013379eb8d5b9dc4a4b562bd00191d
Martin-rev <corp.revillon@hubebi.com>
2025-05-07 18:36:45 +0200

    add path module

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,11 @@
 import streamlit as st
-import os
+import path
+import sys
 
 #Global Variable
+dir = path.Path(__file__).absolute()
+sys.path.append(dir.parent.parent)
+
 st.session_state.RESULT_FILE = "Result_docking\\"
 st.session_state.DOCKING_FILE = "Fichier docking\\"
 
